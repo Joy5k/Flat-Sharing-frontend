62e315800cb9658956b0f861c2691232e9d2732d
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-05-24 15:53:44 +0600

    created the footer

--- src/app/(withDashboardLayout)/dashboard/admin/appointments/page.tsx
+++
@@ -1,9 +0,0 @@
-const AppointmentsPage = () => {
-  return (
-    <div>
-      <h1>Appointments Page</h1>
-    </div>
-  );
-};
-
-export default AppointmentsPage;
--- src/components/Shared/Footer/Footer.tsx
+++ src/components/Shared/Footer/Footer.tsx
@@ -5,27 +5,210 @@ import facebookIcon from "@/assets/landing_page/facebook.png";
 import instagramIcon from "@/assets/landing_page/instagram.png";
 import twitterIcon from "@/assets/landing_page/twitter.png";
 import linkedIcon from "@/assets/landing_page/linkedin.png";
+import assets from "@/assets";
 
 const Footer = () => {
   return (
-    <Box bgcolor="rgb(17, 26, 34)" py={5}>
+    <Box bgcolor="#43bcce" py={5}>
       <Container>
-        <Stack direction="row" gap={4} justifyContent="center">
-          <Typography color="#fff" component={Link} href="/consultation">
-            Consultation
-          </Typography>
-          <Typography color="#fff">Health Plans</Typography>
-          <Typography color="#fff">Medicine</Typography>
-          <Typography color="#fff">Diagnostics</Typography>
-          <Typography color="#fff">NGOs</Typography>
-        </Stack>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Stack>
+            <Typography color="white">New to sharing</Typography>
+            <Typography
+              component={Link}
+              color="white"
+              href="/"
+              sx={{
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Why flat share?
+            </Typography>
+            <Typography
+              component={Link}
+              color="white"
+              href="/"
+              sx={{
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              The Rent a Room scheme
+            </Typography>
+            <Typography
+              component={Link}
+              color="white"
+              href="/"
+              sx={{
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Guide to taking in a lodger
+            </Typography>
+            <Typography
+              component={Link}
+              color="white"
+              href="/"
+              sx={{
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Landlords guide to renting by the room
+            </Typography>
+            <Typography
+              component={Link}
+              color="white"
+              href="/"
+              sx={{
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Shared Home Insurance
+            </Typography>
+            <Typography
+              component={Link}
+              color="white"
+              href="/"
+              sx={{
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Tenancy Agreements
+            </Typography>
+          </Stack>
 
-        <Stack direction="row" gap={2} justifyContent="center" py={3}>
-          <Image src={facebookIcon} width={30} height={30} alt="facebook" />
-          <Image src={instagramIcon} width={30} height={30} alt="facebook" />
-          <Image src={twitterIcon} width={30} height={30} alt="facebook" />
-          <Image src={linkedIcon} width={30} height={30} alt="facebook" />
-        </Stack>
+          <Stack>
+            <Typography color="white">Our services</Typography>
+            <Typography
+              component={Link}
+              color="white"
+              href="/"
+              sx={{
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Advertise a room
+            </Typography>
+            <Typography
+              component={Link}
+              color="white"
+              href="/"
+              sx={{
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Post a room wanted ad
+            </Typography>
+            <Typography
+              component={Link}
+              color="white"
+              href="/"
+              sx={{
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Advertise a whole property
+            </Typography>
+            <Typography
+              component={Link}
+              color="white"
+              href="/"
+              sx={{
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Student Accommodation
+            </Typography>
+            <Typography
+              component={Link}
+              color="white"
+              href="/"
+              sx={{
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Monday to Friday lets
+            </Typography>
+          </Stack>
+          <Stack>
+            <Typography component="p" color="white">
+              contact us
+            </Typography>
+            <Typography component="p" color="white">
+              Mon to Fri: 9am _ 8.30pm
+            </Typography>
+            <Typography component="p" color="white">
+              Weekends: 10am _ 7.30pm
+            </Typography>
+            <Typography component="p" color="white">
+              Email:mmehedihasanjoyv@gmail.com
+            </Typography>
+            <Typography component="p" color="white">
+              Phone:+8801601588531
+            </Typography>
+            <Typography component="p" color="white" ml={-14}>
+              <Stack direction="row" gap={2} justifyContent="center" py={-2}>
+                <Image
+                  src={facebookIcon}
+                  width={30}
+                  height={30}
+                  alt="facebook"
+                />
+                <Image
+                  src={instagramIcon}
+                  width={30}
+                  height={30}
+                  alt="facebook"
+                />
+                <Image
+                  src={twitterIcon}
+                  width={30}
+                  height={30}
+                  alt="facebook"
+                />
+                <Image src={linkedIcon} width={30} height={30} alt="facebook" />
+              </Stack>
+            </Typography>
+          </Stack>
+        </Box>
+
         {/* <div className="border-b-[1px] border-dashed"></div> */}
         <Box
           sx={{
@@ -41,23 +224,13 @@ const Footer = () => {
           py={3}
         >
           <Typography component="p" color="white">
-            &copy;2024 Ph HealthCare. All Rights Reserved.
+            &copy;2024 Spare Room. All Rights Reserved.
           </Typography>
-          <Typography
-            variant="h4"
-            component={Link}
-            href="/"
-            fontWeight={600}
-            color="white"
-          >
-            P
-            <Box component="span" color="primary.main">
-              H
-            </Box>{" "}
-            Health Care
+          <Typography component={Link} href="/" fontWeight={600} color="white">
+            <Image src={assets.svgs.logo} alt="Logo" width={300} height={300} />
           </Typography>
-          <Typography component="p" color="white">
-            Privacy Policy! Terms & Conditions
+          <Typography component={Link} color="white" href="/">
+            Terms of Use, Privacy Policy, etc.
           </Typography>
         </Stack>
       </Container>
